8d02f8a915f3ed60eb1dff19d49d5a9dbce5d1fd
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-05-30 20:55:27 -0500

    fix

---
+++ src/main/java/com/example/VeterinariaApp/VeterinariaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.VeterinariaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VeterinariaApp/VeterinariaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VeterinariaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
