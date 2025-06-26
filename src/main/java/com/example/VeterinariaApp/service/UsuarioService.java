629017f5c1f6f08e5d41a8dedd237238c49e92c1
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-06-25 23:07:36 -0500

    [VT-14]: Encriptar contraseña con BCrypt.

---
+++ src/main/java/com/example/VeterinariaApp/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.example.VeterinariaApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.VeterinariaApp.Dto.UsuarioDTO;
+import com.example.VeterinariaApp.entities.Rol;
+import com.example.VeterinariaApp.entities.Usuario;
+import com.example.VeterinariaApp.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+     @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario registrar(UsuarioDTO dto) {
+        if (usuarioRepository.existsByEmail(dto.getEmail())) {
+            throw new RuntimeException("El email ya está registrado");
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre(dto.getNombre());
+        usuario.setEmail(dto.getEmail());
+        usuario.setPassword(passwordEncoder.encode(dto.getPassword()));
+        usuario.setRol(Rol.CLIENTE);
+
+        return usuarioRepository.save(usuario);
+    }
+
+}
