5c5bdec31906e0f60a90e83018c51a8cf05101b8
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-07-20 01:24:46 -0500

    Deploy

--- src/main/java/com/example/VeterinariaApp/VeterinariaAppApplication.java
+++ src/main/java/com/example/VeterinariaApp/VeterinariaAppApplication.java
@@ -2,6 +2,10 @@ package com.example.VeterinariaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VeterinariaAppApplication {
@@ -10,4 +14,19 @@ public class VeterinariaAppApplication {
 		SpringApplication.run(VeterinariaAppApplication.class, args);
 	}
 
+	@Configuration
+	public static class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+				}
+			};
+		}
+		
+	}
 }
